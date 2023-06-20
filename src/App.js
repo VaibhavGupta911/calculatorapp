2155e362bc30e0dc380230a19f95a09142c8f9fd
VaibhavGupta911 <guptaayush519@gmail.com>
2023-06-20 19:09:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  useState,
+  useRef
+} from "react";
+
+import "./App.css";
 
 function App() {
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+  const [result, setResult] = useState(0);
+  {/*Here e is event and preventDefault is used to prevent refresh of browser*/ }
+
+  function plus(e) {
+    e.preventDefault();
+    setResult((result) => result + Number(inputRef.current.value));
+  };
+
+  function minus(e) {
+    // Add the code for the minus function 
+    e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value));
+  };
+
+  function times(e) {
+    // Add the code for the plus function 
+    e.preventDefault();
+    setResult((result) => result * Number(inputRef.current.value));
+  };
+
+  function divide(e) {
+    // Add the code for the divide function 
+    e.preventDefault();
+    setResult((result) => result / Number(inputRef.current.value));
+  };
+
+  function resetInput(e) {
+    // Add the code for the resetInput function 
+    e.preventDefault();
+    inputRef.current.value = 0;
+
+  };
+
+  function resetResult(e) {
+    // Add the code for the resetResult function 
+    e.preventDefault();
+    setResult(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Simplest Working Calculator</h1>
+      </div>
+      <form>
+        <p ref={resultRef}>
+          {/* add the value of the current total */}
+          {result}
+        </p>{/*Here linking result  with result Ref */}
+        <input
+          pattern="[0-9]"
+          ref={inputRef}
+          type="number"
+          placeholder="Type a number"
+        />{/*Here linking input with input Ref */}
+        <button onClick={plus}>add</button>
+        {/* Add the subtract button */}
+        <button onClick={minus}>subtract</button>
+        {/* Add the multiply button */}
+        <button onClick={times}>multiply</button>
+        {/* Add the divide button */}
+        <button onClick={divide}>divide</button>
+        {/* Add the resetInput button */}
+        <button onClick={resetInput}>reset input</button>
+        {/* Add the resetResult button */}
+        <button onClick={resetResult}>reset result</button>
+      </form>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
